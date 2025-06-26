4a25c0c0f17142606a21039aa7fa84899af22676
Chirag Patil <patilchirag192k@gmail.com>
2025-06-26 12:13:03 +0530

    claculating employee wage

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -2,26 +2,31 @@
 
 namespace EmployeeWage
 {
-    class EmpolyeeWage
+    class EmployeeWage
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to employee wage problem");
+        //Constants value cannot be change
+        const int IS_PRESENT = 1;
+        const int WAGE_PER_HOUR = 20;
+        const int FULL_DAY_HOURS = 8;
 
-            const int IS_PRESENT = 1;
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the employee wage problem");
 
             Random random = new Random();
-            int empCheck = random.Next(0, 2); // 0 or 1
-
+            int empCheck = random.Next(0, 2);
             if (empCheck == IS_PRESENT)
             {
                 Console.WriteLine("Employee is Present");
+
+                int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
+                Console.WriteLine("Employee Daily Wage: ₹" + dailyWage);
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                Console.WriteLine("Employee Daily Wage: Rs.0");
             }
-
         }
     }
 }
