067614b4169a549da8eda8bf2f8848d0fd30ee31
Chirag Patil <patilchirag192k@gmail.com>
2025-06-26 15:15:14 +0530

    Calculating-monthly-employee-wage

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -9,37 +9,46 @@ namespace EmployeeWage
         const int IS_FULL_TIME = 1;
         const int IS_PART_TIME = 2;
         const int WAGE_PER_HOUR = 20;
+        const int NUM_OF_WORKING_DAYS = 20;
 
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the employee wage problem");
 
+            int totalWage = 0;
+
             Random random = new Random();
-            int empCheck = random.Next(0, 3);
 
-            int empHours = 0;
-
-            switch (empCheck)
+            for (int day = 1; day <= NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_FULL_TIME:
-                    Console.WriteLine("Employee is Present (Full-Time)");
-                    empHours = 8;
-                    break;
+                int empCheck = random.Next(0, 3);
+                int empHours = 0;
 
-                case IS_PART_TIME:
-                    Console.WriteLine("Employee is Present (Part-Time)");
-                    empHours = 4;
-                    break;
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHours = 8;
+                        Console.WriteLine("Day " + day + ": Full-Time (8 hrs)");
+                        break;
 
-                case IS_ABSENT:
-                default:
-                    Console.WriteLine("Employee is Absent");
-                    empHours = 0;
-                    break;
+                    case IS_PART_TIME:
+                        empHours = 4;
+                        Console.WriteLine("Day " + day + ": Part-Time (4 hrs)");
+                        break;
+
+                    case IS_ABSENT:
+                    default:
+                        empHours = 0;
+                        Console.WriteLine("Day " + day + ": Absent (0 hrs)");
+                        break;
+                }
+
+                int dailyWage = empHours * WAGE_PER_HOUR;
+                totalWage += dailyWage;
+                Console.WriteLine("Day " + day + " Wage: Rs." + dailyWage + "\n");
             }
 
-            int dailyWage = empHours * WAGE_PER_HOUR;
-            Console.WriteLine("Employee Daily Wage: Rs." + dailyWage);
+            Console.WriteLine("Total Monthly Wage for 20 Days: Rs." + totalWage);
         }
     }
 }
