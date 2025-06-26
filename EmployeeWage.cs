c2d3d1d2ab6563e607a0531f1aa54d598dc45891
Chirag Patil <patilchirag192k@gmail.com>
2025-06-26 12:01:06 +0530

    Checking employee presence

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -8,6 +8,20 @@ namespace EmployeeWage
         {
             Console.WriteLine("Welcome to employee wage problem");
 
+            const int IS_PRESENT = 1;
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2); // 0 or 1
+
+            if (empCheck == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
         }
     }
 }
