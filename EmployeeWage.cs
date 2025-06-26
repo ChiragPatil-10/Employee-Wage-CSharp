49c59379c0f41ed6fc855ad3e9149b617b8525d5
Chirag Patil <patilchirag192k@gmail.com>
2025-06-26 14:36:03 +0530

    UC3-Added-Part-time-Employee

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -4,29 +4,38 @@ namespace EmployeeWage
 {
     class EmployeeWage
     {
-        //Constants value cannot be change
-        const int IS_PRESENT = 1;
+        // Constants
+        const int IS_ABSENT = 0;
+        const int IS_FULL_TIME = 1;
+        const int IS_PART_TIME = 2;
         const int WAGE_PER_HOUR = 20;
-        const int FULL_DAY_HOURS = 8;
 
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the employee wage problem");
 
             Random random = new Random();
-            int empCheck = random.Next(0, 2); 
-            if (empCheck == IS_PRESENT)
-            {
-                Console.WriteLine("Employee is Present");
+            int empCheck = random.Next(0, 3); 
+            int empHours = 0;
 
-                int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
-                Console.WriteLine("Employee Daily Wage: ₹" + dailyWage);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present (Full-Time)");
+                empHours = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee is Present (Part-Time)");
+                empHours = 4;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
-                Console.WriteLine("Employee Daily Wage: Rs.0");
+                empHours = 0;
             }
+
+            int dailyWage = empHours * WAGE_PER_HOUR;
+            Console.WriteLine("Employee Daily Wage: Rs." + dailyWage);
         }
     }
 }
